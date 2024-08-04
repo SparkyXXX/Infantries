da29efbb9ed53b6e5f2b40b3b930f73c7a8a4cd1
Hatrix <3113624526@qq.com>
2024-08-04 11:23:27 +0800

    加入场地训练的测试宏定义

--- Chassis/User/Infantry/test_ctrl.h
+++ Chassis/User/Infantry/test_ctrl.h
@@ -4,7 +4,7 @@
 #define STAR_DUST 4
 #define HUGE_TITAN 5
 #define WHITE_MISTRESS 6
-#define ROBOT_ID WHITE_MISTRESS
+#define ROBOT_ID STAR_DUST
 
 #define NO_SYS_IDENT 1
 #define SYS_IDENT 0
@@ -17,3 +17,10 @@
 #define YAW_REMOTE 1
 #define YAW_STEP 0
 #define YAW_MOVE YAW_REMOTE
+
+#define POWER_INFINITE 0
+#define POWER_350 1
+#define POWER_300 2
+#define POWER_250 3
+#define POWER_200 4
+#define TMD_POWER_CTRL POWER_300
